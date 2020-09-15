02f93fe01061eb92814bb90b92a737c34d543adb
yuchuan zhuang <yuchuan.zhuang@mindgeek.com>
2020-09-14 20:52:33 -0400

    Done walkthrough

---
+++ src/button.js
@@ -0,0 +1,15 @@
+// take a string, the button lable and return a element
+
+// module.exports = (buttonName) => {
+//     return `Button: ${buttonName}`;
+// };
+/**
+ * 
+ * @param {string} buttonName 
+ * @returns {Element}
+ */
+const makeButton = buttonName => {
+    return `Button: ${buttonName}`;
+};
+
+export default makeButton;
